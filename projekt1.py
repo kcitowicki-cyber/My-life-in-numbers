d12068ad2b816b6d0bd52b539f8d49045e7e955f
kcitowicki-cyber <k.citowicki@gmail.com>
2026-02-16 22:06:58 +0100

    Implement age calculation and print details
    Calculates and displays age in months, days, and hours.

---
+++ projekt1.py
@@ -0,0 +1,10 @@
+name = 'Krystian'
+age = 37
+months = age * 12
+days = age * 365
+hours = days * 24
+print("imię:",name)
+print("wiek:",age)
+print("przeżyte miesiące",months)
+print("przeżyte dni",days)
+print("przeżyte godziny",hours
